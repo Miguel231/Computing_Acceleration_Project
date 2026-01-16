d02565830cd9708e650343c76a910ef0b1051638
Miguel Arpa Robig <31631526+Miguel231@users.noreply.github.com>
2026-01-16 14:41:37 +0100

    Delete model_part directory

--- model_part/check_mouse.py
+++
@@ -1,8 +0,0 @@
-from evdev import InputDevice, ecodes
-
-dev = InputDevice("/dev/input/by-id/usb-Compx_2.4G_Receiver-if01-event-mouse")
-print("Listening for right click...")
-
-for event in dev.read_loop():
-    if event.type == ecodes.EV_KEY and event.code == ecodes.BTN_RIGHT:
-        print("Right button:", event.value)
